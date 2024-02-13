16f19ada046f5c278f8c8c02561c1feb5b7b0028
coxmd <coxmusyoki@gmail.com>
2024-02-13 16:05:30 +0300

    Added razor component payment success

--- BlazorStripePayment/obj/Debug/net8.0/BlazorStripePayment.AssemblyInfo.cs
+++ BlazorStripePayment/obj/Debug/net8.0/BlazorStripePayment.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorStripePayment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffe63db8d1d81bcc67deb5c2e03b94762a10f818")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+009aa3f8462a6026d70c13f4722fb46b359ee016")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorStripePayment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorStripePayment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
